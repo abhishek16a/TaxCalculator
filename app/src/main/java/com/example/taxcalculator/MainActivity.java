756d83254922c11694e1e8716708c69dfa8f7f79
abhishek16a <abhishekadhikari16a@gmail.com>
2019-04-11 18:11:56 +0545

    making other class to ose its object

--- app/src/main/java/com/example/taxcalculator/MainActivity.java
+++ app/src/main/java/com/example/taxcalculator/MainActivity.java
@@ -8,16 +8,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnCalculate;
+    private TextView tvOut,tvTaxableAmount;
+    private EditText etAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        btnCalculate=findViewById(R.id.btnCalculate);
+        tvTaxableAmount=findViewById(R.id.tvTaxAmount);
+        etAmount=findViewById(R.id.etSalary);
+        tvOut=findViewById(R.id.tvTotal);
 
+        btnCalculate.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        int id = v.getId();
+        if(id == R.id.btnCalculate)
+        {
+            if(!validate()){
+                return;
+            }
+            double salary=Double.parseDouble(etAmount.getText().toString());
+
+            TaxCalculator_abhishek taxCalculator_abhishek=new TaxCalculator_abhishek();
+            taxCalculator_abhishek.setAmount(salary);
+            Double tax=taxCalculator_abhishek.taxableAmount();
+            tvTaxableAmount.setText(Double.toString(tax));
+            double annualsalary=salary*12;
+            double totalsalary=annualsalary-tax;
+            tvOut.setText(Double.toString(totalsalary));
+        }
+    }
+    public boolean validate(){
+        boolean isValid=true;
+        if(TextUtils.isEmpty(etAmount.getText().toString())){
+            etAmount.setError("Please Enter Your Salary");
+            etAmount.requestFocus();
+            isValid=false;
+        }return isValid;
+    }
 }
+
+
 
 
---
+++ app/src/main/java/com/example/taxcalculator/TaxCalculator_abhishek.java
@@ -0,0 +1,48 @@
+package com.example.taxcalculator;
+
+public class TaxCalculator_abhishek {
+    double salary;
+    double mintax=0.01;
+    double midtax= 0.15;
+    double maxtax= 0.25;
+
+    public double getAmount() {
+        return salary;
+    }
+
+    public void setAmount(double salary) {
+        this.salary = salary;
+    }
+
+    public double taxableAmount(){
+        double amount=salary*12;
+        double grandPayableTax = 0;
+        if(amount<=200000){
+            double taxamount=amount*mintax;
+            return grandPayableTax=taxamount;
+        }
+        else if (amount>200000 && amount<=350000){
+            double midAmount=200000;
+            double newamount=amount-midAmount;
+            double taxableamount=newamount*midtax;
+            double newamount2=amount-newamount;
+            double midTax=newamount2*mintax;
+            double totaltaxamount=taxableamount+midTax;
+            return grandPayableTax=totaltaxamount;
+        }
+        else if(amount>350000) {
+            double minAmount = 200000;
+            double mustPayTax=minAmount*mintax;
+            double midAmount=150000;
+            double midPayTax=midAmount*0.015;
+            double maxPayamount=amount-350000;
+            double maxPayTax=maxPayamount*maxtax;
+
+            double totaltaxamount = mustPayTax + midPayTax +maxPayTax;
+            return grandPayableTax = totaltaxamount;
+
+        }
+        return grandPayableTax;
+    }
+}
+
